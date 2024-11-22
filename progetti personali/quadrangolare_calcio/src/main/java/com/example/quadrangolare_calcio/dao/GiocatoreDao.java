19f4220815db2c198594a413baa9c8f7ab17843a
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2024-11-22 22:21:35 +0100

    Costruzione del package dao

---
+++ progetti personali/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/dao/GiocatoreDao.java
@@ -0,0 +1,7 @@
+package com.example.quadrangolare_calcio.dao;
+
+import com.example.quadrangolare_calcio.model.Giocatore;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GiocatoreDao extends CrudRepository<Giocatore, Integer> {
+}
