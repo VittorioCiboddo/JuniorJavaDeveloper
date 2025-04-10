173123823a01fb3f2fb99071b01b932ce2fe3f03
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2025-04-10 17:25:45 +0200

    inizio back-end per rose-complete.html

---
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/DettaglioGiocatoriController.java
@@ -0,0 +1,11 @@
+package com.example.quadrangolare_calcio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dettagliogiocatori")
+public class DettaglioGiocatoriController {
+
+
+}
