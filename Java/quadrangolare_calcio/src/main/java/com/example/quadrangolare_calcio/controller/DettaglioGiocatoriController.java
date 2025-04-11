8a887e98004a904708fc2633d922e02c3db48396
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2025-04-11 19:36:46 +0200

    costruzione pagina dettaglio giocatori e inserimento icone grafiche

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/DettaglioGiocatoriController.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/DettaglioGiocatoriController.java
@@ -1,11 +1,30 @@
 package com.example.quadrangolare_calcio.controller;
 
+import com.example.quadrangolare_calcio.model.Giocatore;
+import com.example.quadrangolare_calcio.service.GiocatoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/dettagliogiocatori")
 public class DettaglioGiocatoriController {
 
+    @Autowired
+    private GiocatoreService giocatoreService;
+
+    @GetMapping
+    public String getDettaglioGiocatore(@RequestParam("id") int idGiocatore, Model model) {
+        Giocatore giocatore = giocatoreService.dettaglioGiocatore(idGiocatore);
+        if (giocatore == null) {
+            return "redirect:/squadre"; // o pagina di errore
+        }
+
+        model.addAttribute("giocatore", giocatore);
+        return "dettaglio-giocatori";
+    }
 
 }
