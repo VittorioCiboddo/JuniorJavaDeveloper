2398c55d732f2a02d301fade7f9d1c33f4963e27
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-06 19:44:10 +0100

    Preparazione controller per pagina di archivio tornei passati

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/ArchivioTorneiController.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/ArchivioTorneiController.java
@@ -1,7 +1,10 @@
 package com.example.quadrangolare_calcio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/archiviotornei")
 public class ArchivioTorneiController {
+
 }
