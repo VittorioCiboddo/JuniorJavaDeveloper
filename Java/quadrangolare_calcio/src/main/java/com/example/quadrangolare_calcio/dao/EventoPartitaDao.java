4d215f7d4f287a6f498d67359073184c7a4c417c
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-09 23:41:58 +0100

    Aggiunta DAO delle nuove entity: - ArchivioGiocatore; - ArchivioSquadra; - EventoPartita; - Partita; - TabellinoPartita; - TipoPartita; - Torneo.

---
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/dao/EventoPartitaDao.java
@@ -0,0 +1,7 @@
+package com.example.quadrangolare_calcio.dao;
+
+import com.example.quadrangolare_calcio.model.EventoPartita;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoPartitaDao extends CrudRepository<EventoPartita, Long> {
+}
