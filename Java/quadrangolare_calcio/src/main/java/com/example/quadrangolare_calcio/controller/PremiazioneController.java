2ac8200ad2a59031c73fba4410a36236c0fd87c7
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-17 14:17:31 +0100

    Aggiunto Controller per premiazione.html

---
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/PremiazioneController.java
@@ -0,0 +1,16 @@
+package com.example.quadrangolare_calcio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/premiazione")
+public class PremiazioneController {
+
+    @GetMapping
+    public String getPage() {
+        return "premiazione";
+    }
+
+}
