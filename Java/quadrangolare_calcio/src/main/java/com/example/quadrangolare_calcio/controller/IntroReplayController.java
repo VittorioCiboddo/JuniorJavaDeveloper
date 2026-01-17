457cf7a8999b86963eda7ef31700b4893831822d
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-18 00:50:38 +0100

    Aggiunto controller per intro-replay.html

---
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/IntroReplayController.java
@@ -0,0 +1,16 @@
+package com.example.quadrangolare_calcio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/intro-replay")
+public class IntroReplayController {
+
+    @GetMapping
+    public String getPage() {
+        return "intro-replay";
+    }
+
+}
