b7be459647563a96e7537fd42ebaa70ae7bab802
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-17 23:18:24 +0100

    Aggiunto controller per il template chi-siamo.html

---
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/ChiSiamoController.java
@@ -0,0 +1,17 @@
+package com.example.quadrangolare_calcio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contatti")
+public class ChiSiamoController {
+
+    @GetMapping()
+    public String getPage() {
+
+        return "chi-siamo";
+    }
+
+}
