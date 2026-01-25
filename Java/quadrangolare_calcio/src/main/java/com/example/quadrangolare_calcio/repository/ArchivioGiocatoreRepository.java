fd00c63c1e0b69145f04cd692ad35233f4a6bb21
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-25 01:13:09 +0100

    Aggiunto il metodo findByGiocatore(Giocatore) per la ricerca dell'intero oggetto Giocatore

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/repository/ArchivioGiocatoreRepository.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/repository/ArchivioGiocatoreRepository.java
@@ -1,6 +1,7 @@
 package com.example.quadrangolare_calcio.repository;
 
 import com.example.quadrangolare_calcio.model.ArchivioGiocatore;
+import com.example.quadrangolare_calcio.model.Giocatore;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +9,7 @@ import java.util.Optional;
 public interface ArchivioGiocatoreRepository extends JpaRepository<ArchivioGiocatore, Long> {
 
     Optional<ArchivioGiocatore> findByGiocatoreIdGiocatore(int idGiocatore);
+
+    Optional<ArchivioGiocatore> findByGiocatore(Giocatore giocatore);
 }
 
