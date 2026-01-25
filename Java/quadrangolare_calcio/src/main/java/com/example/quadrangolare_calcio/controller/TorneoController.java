c5a542f7834c8c1ffb32131c43cf06d13623ce81
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-25 01:07:49 +0100

    Aggiunta chiamata Post per salvare l'intero torneo sul db attraverso il metodo nel service .salvaTorneoIntero()

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/TorneoController.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/TorneoController.java
@@ -1,13 +1,11 @@
 package com.example.quadrangolare_calcio.controller;
 
+import com.example.quadrangolare_calcio.dto.TorneoSalvataggioDTO;
 import com.example.quadrangolare_calcio.model.Torneo;
 import com.example.quadrangolare_calcio.service.TorneoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -20,6 +18,16 @@ public class TorneoController {
     private TorneoService torneoService;
 
     // --- GESTIONE TORNEO ---
+
+    @PostMapping("/salva-completo")
+    public ResponseEntity<String> salvaTorneoCompleto(@RequestBody TorneoSalvataggioDTO dto) {
+        try {
+            torneoService.salvaTorneoIntero(dto);
+            return ResponseEntity.ok("Torneo salvato con successo!");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Errore durante il salvataggio: " + e.getMessage());
+        }
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Torneo> getDettaglioTorneo(@PathVariable Long id) {
