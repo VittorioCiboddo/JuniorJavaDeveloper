d61619e2277fd773d0673bdc5afbca3c68f2678d
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-25 01:17:38 +0100

    Aggiunto il metodo findBySquadra(Squadra) per la ricerca dell'intero oggetto Squadra

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/repository/ArchivioSquadraRepository.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/repository/ArchivioSquadraRepository.java
@@ -1,6 +1,7 @@
 package com.example.quadrangolare_calcio.repository;
 
 import com.example.quadrangolare_calcio.model.ArchivioSquadra;
+import com.example.quadrangolare_calcio.model.Squadra;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +9,7 @@ import java.util.Optional;
 public interface ArchivioSquadraRepository extends JpaRepository<ArchivioSquadra, Long> {
 
     Optional<ArchivioSquadra> findBySquadraIdSquadra(int idSquadra);
+
+    Optional<ArchivioSquadra> findBySquadra(Squadra squadra);
 
 }
