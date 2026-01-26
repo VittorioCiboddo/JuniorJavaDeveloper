24b65b80d76dd10f3546e798405250831781bda4
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-26 16:04:28 +0100

    Aggiunto metodo booleano per la gestione dinamica della presenza di torni salvati sul db o meno

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/repository/TorneoRepository.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/repository/TorneoRepository.java
@@ -4,4 +4,6 @@ import com.example.quadrangolare_calcio.model.Torneo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TorneoRepository extends JpaRepository<Torneo, Long> {
+
+    boolean existsBy();
 }
