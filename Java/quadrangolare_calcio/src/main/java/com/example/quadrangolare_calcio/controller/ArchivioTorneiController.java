d2dd9a130dc578227c2000e5905bb96724e6a939
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-26 19:56:16 +0100

    Aggiunto il model per favorire il popolamento di archivio-tornei.html

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/ArchivioTorneiController.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/ArchivioTorneiController.java
@@ -1,6 +1,7 @@
 package com.example.quadrangolare_calcio.controller;
 
 import com.example.quadrangolare_calcio.repository.TorneoRepository;
+import com.example.quadrangolare_calcio.service.TorneoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,12 +15,21 @@ public class ArchivioTorneiController {
     @Autowired
     private TorneoRepository torneoRepository;
 
+    @Autowired
+    private TorneoService torneoService;
+
     @GetMapping
     public String getPage(Model model) {
-
-        boolean esistonoTornei = torneoRepository.existsBy();
+        boolean esistonoTornei = !torneoRepository.findAll().isEmpty();
         model.addAttribute("esistonoTornei", esistonoTornei);
+
+        if (esistonoTornei) {
+            model.addAttribute("alboDOro", torneoService.getAlboDOro());           // List<Map<String,String>>
+            model.addAttribute("rankingSquadre", torneoService.getMedagliereStorico()); // List<Map<String,Object>>
+            model.addAttribute("hallOfFame", torneoService.getHallOfFame());       // Map<String,Object>
+        }
 
         return "archivio-tornei";
     }
+
 }
