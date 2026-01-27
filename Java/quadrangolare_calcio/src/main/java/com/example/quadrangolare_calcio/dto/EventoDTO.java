6e9fdc4daaf4a94deca54d270299089e6aab758b
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-27 17:53:45 +0100

    Pulizia commenti fuorvianti

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/dto/EventoDTO.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/dto/EventoDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class EventoDTO {
     private int idGiocatore;
-    private String tipoEvento; // es. "GOL"
+    private String tipoEvento;
     private String esitoEvento;
     private int minuto;
 }
