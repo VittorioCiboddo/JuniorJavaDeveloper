8c2dccfad940c34e03d6544c929aa843f002129d
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-29 23:33:04 +0100

    Aggiunto metodo countByTorneoIdAndSquadra() con query nativa per contare il numero di partite giocate nello stesso torneo per squadra

--- Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/repository/PartitaRepository.java
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/repository/PartitaRepository.java
@@ -2,11 +2,16 @@ package com.example.quadrangolare_calcio.repository;
 
 import com.example.quadrangolare_calcio.model.Partita;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PartitaRepository extends JpaRepository<Partita, Long> {
 
     List<Partita> findByTorneoIdTorneo(int idTorneo);
+
+    @Query("SELECT COUNT(p) FROM Partita p WHERE p.torneo.idTorneo = :idTorneo AND (p.squadraHome.nome = :nomeSquadra OR p.squadraAway.nome = :nomeSquadra)")
+    int countByTorneoIdAndSquadra(@Param("nomeSquadra") String nomeSquadra, @Param("idTorneo") int idTorneo);
 
 }
