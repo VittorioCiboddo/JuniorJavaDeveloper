700e20a0f66c9bd05aa7d0cb9cf78b7c2720c48c
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2026-01-29 23:33:40 +0100

    Aggiunto controller per gestire la pagina dettaglio-torneo.html

---
+++ Java/quadrangolare_calcio/src/main/java/com/example/quadrangolare_calcio/controller/DettaglioTorneoController.java
@@ -0,0 +1,38 @@
+package com.example.quadrangolare_calcio.controller;
+
+import com.example.quadrangolare_calcio.model.Torneo;
+import com.example.quadrangolare_calcio.service.TorneoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dettagliotorneo")
+public class DettaglioTorneoController {
+
+    @Autowired
+    private TorneoService torneoService;
+
+    @GetMapping("/{idTorneo}")
+    public String getDettaglioTorneo(
+            @PathVariable Long idTorneo,
+            Model model
+    ) {
+
+        Torneo torneo = torneoService.getDettaglioTorneo(idTorneo);
+
+        model.addAttribute("torneo", torneo);
+        model.addAttribute("stats", torneoService.getStatsTorneo(idTorneo.intValue()));
+        model.addAttribute("classifica", torneoService.getClassificaTorneoDettagliata(idTorneo.intValue()));
+        model.addAttribute("miniTabellone", torneoService.getMiniTabellone(idTorneo.intValue()));
+
+
+        return "dettaglio-torneo";
+    }
+
+
+
+}
